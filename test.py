e2cf8f60fb19b11c5af146006dde80fa5150b12f
anita <slater.anita@gmail.com>
2018-07-23 23:42:56 -0400

    [addition] Adding test file to determine if load cell works

---
+++ test.py
@@ -0,0 +1,23 @@
+def readCount():
+  i=0
+  Count=0
+  gpio.setup(DT, gpio.OUT)
+  gpio.output(DT,1)
+  gpio.output(SCK,0)
+  gpio.setup(DT, gpio.IN)
+
+  while gpio.input(DT) == 1:
+      i=0
+  for i in range(24):
+        gpio.output(SCK,1)
+        Count=Count<<1
+
+        gpio.output(SCK,0)
+        #time.sleep(0.001)
+        if gpio.input(DT) == 0: 
+            Count=Count+1
+        
+  gpio.output(SCK,1)
+  Count=Count^0x800000
+  gpio.output(SCK,0)
+  return Count 
